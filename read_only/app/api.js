a211c0f0656546a9cf23eba8c148b1ec3297ea76
jaehakl <jaehak@qutat.net>
2026-02-20 00:46:18 +0900

    Update api.js

--- read_only/app/api.js
+++ read_only/app/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const API_URL =
   process.env.VITE_API_BASE_URL ||
-  "http://localhost:8000";
+  "";
 axios.defaults.withCredentials = true;
 
 const authReadyGate = {
